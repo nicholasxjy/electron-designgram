46b7ffd9f8e00a166062183996f9fdefd46af52f
nicholasxjy <yitaqi2011@gmail.com>
2016-10-19 09:33:21 +0800

    add elementui

--- app/src/main.js
+++ app/src/main.js
@@ -5,20 +5,25 @@ import Router from 'vue-router'
 import App from './App'
 import routes from './routes'
 
+import Element from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
+
+
 import '../node_modules/normalize-css/normalize.css'
 import './assets/app.css'
 
 Vue.use(Electron);
-Vue.use(Router)
-Vue.config.debug = true
+Vue.use(Router);
+Vue.use(Element);
+Vue.config.debug = true;
 
 const router = new Router({
   scrollBehavior: () => ({ y: 0 }),
   routes
-})
+});
 
 /* eslint-disable no-new */
 new Vue({
   router,
   ...App
-}).$mount('#app')
+}).$mount('#app');
